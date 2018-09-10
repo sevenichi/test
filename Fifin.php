5d7438720974d6076cf2a1793a84c4140fc779ff
sevenichi <34051479+sevenichi@users.noreply.github.com>
2018-09-10 14:47:04 +0700

    Update Fifin.php

--- Fifin.php
+++ Fifin.php
@@ -1,12 +1,12 @@
 <?php
 	 //php 7.0
- 	echo "<html>" ;
- 	echo "<head>" ;
- 	echo "<title> html dlam php </title>" ;
- 	echo "</head>" ;
- 	echo "<body>" ;
- 	echo "blablablablabla" ;
- 	echo "</body>" ;
- 	echo "</html>" ;
+ 	echo "<html>";
+ 	echo "<head>";
+ 	echo "<title> html dlam php </title>";
+ 	echo "</head>";
+ 	echo "<body>";
+ 	echo "blablablablabla";
+ 	echo "</body>";
+ 	echo "</html>";
 
  ?>
